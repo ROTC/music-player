551b0a59c824db92f3706390c4861bdd0ede49f3
丰子小 <2207977085@qq.com>
2014-03-14 23:31:06 +0800

    process of database

--- DBModule.cpp
+++ DBModule.cpp
@@ -67,3 +67,16 @@ bool DBModule::ClearDB()
     QSqlQuery query;
     return query.exec("delete from musiclist");
 }
+
+bool DBModule::DeleteLine(const QString &filename)
+{
+    QSqlQuery query;
+    query.exec("delete from musiclist wherer filename = ?");
+    query.bindValue(filename,QVariant::String);
+    return query.exec();
+}
+
+bool DBModule::InsertLine(const QString &filename)
+{
+    return true;
+}
